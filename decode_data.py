55c948295c995fc81771f6500672c0c25992e0a7
J Chen <jjccforth@gmail.com>
2017-02-19 21:56:02 -0500

    deal with gif file with different scale

--- decode_data.py
+++ decode_data.py
@@ -6,10 +6,13 @@ from ocrhelper import preprocess
 
 #scale = 4
 
-def ocr_top10(img, scale = 4):
+def ocr_top10(img, scale = 4,gif = False):
 
     top10 = []
     (x,y,w,h) = ( 20,90,298,115)
+    if gif:
+        (x,y,w,h) = (16, 78, 239, 99)
+
     crop_img = img[y: y + h, x: x + w]
 
     img = preprocess(crop_img, 10)
@@ -22,9 +25,13 @@ def ocr_top10(img, scale = 4):
     for j in range(0, 2):
         offset = j
         for i in range (0,5):
-            blockimg3 = lut_bw[offset + setph * j:offset + setph * (j + 1), i + stepw * i: i + stepw * (i + 1) - 4]
-            #cv2.imwrite("data/bocr%d.jpg" % num, blockimg3)
-            img = Image.fromarray(blockimg3)
+            blockimg3 = lut_bw[offset + setph * j:offset + setph * (j + 1)-6, i + stepw * i: i + stepw * (i + 1) - 8]
+
+            #added 5 column to left of cropped image
+            image_add = np.full((blockimg3.shape[0], blockimg3.shape[1] + 10), 255,np.uint8)
+            image_add[:, 7:-3] = blockimg3
+            cv2.imwrite("data/bocr%d.jpg" % num, image_add)
+            img = Image.fromarray(image_add)
             text = pytesseract.image_to_string(img)
             #print  text
             num += 1
@@ -32,9 +39,19 @@ def ocr_top10(img, scale = 4):
     return top10
 
 if __name__=="__main__":
+    file = 'IKForecast_under_10_17_Feb_2016-one-year-long-until-17-Feb.gif'
+    #file  = 'input/IKForecast_BIOTECH_13_Jan_2017-30-days-long-until-13-Feb-sticky.jpg'
+    is_gif = False
+    if file.endswith('f'):
+        is_gif = True
 
-    img = cv2.imread('input/IKForecast_BIOTECH_13_Jan_2017-30-days-long-until-13-Feb-sticky.jpg')
+    if(is_gif):
+        imgA = Image.open(file)
+        imgB = imgA.convert('RGB')
+        img = np.asarray(imgB)
+    else:
+        img = cv2.imread(file)
 
-    ocr_res = ocr_top10(img)
+    ocr_res = ocr_top10(img,gif = is_gif)
     for block in ocr_res:
-        print block
+        print block, "\n<<<<"
