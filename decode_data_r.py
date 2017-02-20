518aaa8193f6442a6e828fdbeff1f21d75d1993f
J Chen <jjccforth@gmail.com>
2017-02-19 22:46:28 -0500

    gif process at right pane

--- decode_data_r.py
+++ decode_data_r.py
@@ -14,19 +14,41 @@ from ocrhelper import preprocess
 
 
 if __name__=="__main__":
-    imgOrig = cv2.imread('input/IKForecast_BIOTECH_13_Jan_2017-30-days-long-until-13-Feb-sticky.jpg')
+
+    file = 'IKForecast_under_10_17_Feb_2016-one-year-long-until-17-Feb.gif'
+    #file  = 'input/IKForecast_BIOTECH_13_Jan_2017-30-days-long-until-13-Feb-sticky.jpg'
+    is_gif = False
+    if file.endswith('f'):
+        is_gif = True
+
+    if(is_gif):
+        imgA = Image.open(file)
+        imgB = imgA.convert('RGB')
+        imgOrig = np.asarray(imgB)
+        rect1 = [292,70,290,29]
+        (xl, yl, wl, hl) = (16, 5, 45, 20)
+        (xm, ym, wm, hm) = (180, 8, 54, 13)
+        (xr, yr, wr, hr) = (251, 5, 26, 20)
+    else:
+        imgOrig = cv2.imread(file)
+        rect1 = [364, 80, 364, 34]  # hight should be 33.5
+        (xl,yl,wl,hl) = (25,5,60,23)
+        (xm, ym, wm, hm) = (225, 5, 75, 20)
+        (xr, yr, wr, hr) = (310, 5, 30, 23)
+
     # rect1 = [364, 60, 364, 20]
     # crop_img = imgOrig[rect1[1]: rect1[1] + rect1[3], rect1[0]: rect1[0] + rect1[2]]
     # img = preprocess(crop_img,100,True)
     # text = pytesseract.image_to_string(img)
     # print text
 
-    (xl,yl,wl,hl) = (25,5,60,23)
-    (xm, ym, wm, hm) = (225, 5, 75, 20)
-    (xr, yr, wr, hr) = (310, 5, 30, 23)
+    # (xl,yl,wl,hl) = (25,5,60,23)
+    # (xm, ym, wm, hm) = (225, 5, 75, 20)
+    # (xr, yr, wr, hr) = (310, 5, 30, 23)
 
 
-    rect1 = [364,80,364,34] #hight should be 33.5
+
+    #[292,70,290,29
     crop_img2 = imgOrig[ rect1[1]: rect1[1]+rect1[3], rect1[0]: rect1[0]+rect1[2]]
     cv2.imwrite("data_r/crop_img2.jpg", crop_img2)
 
@@ -46,7 +68,10 @@ if __name__=="__main__":
         #text = pytesseract.image_to_string(img)
         #print "No.%d"%num
         #print text
-        yy += height - 0.3
+        adjust = 0.3
+        if is_gif:
+            adjust = 0.25
+        yy += height - adjust
         y = int(yy)
         ########
         imgl = crop_img2.copy()[yl: yl + hl, xl: xl + wl]
